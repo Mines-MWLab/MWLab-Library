19da69503460d4bd01fea0c269576e0200f797e2
gabsantamaria <gabriel19916@gmail.com>
2025-05-27 14:11:29 -0600

    Add dOR_EOM_DC device

--- scripts/devices.py
+++ scripts/devices.py
@@ -4,6 +4,7 @@ from gplugins.common.config import PATH
 from gdsfactory.typings import CrossSectionSpec
 from lnoi400.spline import bend_S_spline_varying_width
 import numpy as np
+import components as mpl
 
 from functools import partial
 import matplotlib.pyplot as plt
@@ -441,6 +442,184 @@ def dOR_EOM():
 
     return c
 
+
+
+#####################
+# Dual Optically Resonant EOM w/edge couplers and GSG landing (draft design)
+#####################
+@gf.cell
+def dOR_EOM_DC(
+    RT_cross_section: CrossSectionSpec = "xs_rwg3000",
+    DC_cross_section: CrossSectionSpec = "xs_rwg1000",
+    DC_io_wg_sep: float = 25,#15.3,
+    DC_sbend_length: float = 150,
+    DC_central_straight_length: float = 16.92,
+    DC_coupl_wg_sep: float = 0.6,
+    DC_coup_wg_width: float = 1.2,
+    DC_ubend_sep: float = 100,
+    h_racetrack: float = 200.0,
+    ls: float = 1408.0, #3850
+    lextra: float = 364, #205
+    rf_gap:float = 4,
+    rf_central_conductor_width: float = 21.0,
+    h: float = 3.0,
+    r: float = 44.7,
+    t: float = 7.0,
+    s: float = 1.5,
+    c: float = 5.0,
+)-> gf.Component:
+
+    #define constants for race track
+    #h_racetrack = 200.0
+    #ls = 335.0
+    modulation_length = ls
+    
+
+
+    # trail_cpw variables
+    
+    #rf_gap = 4
+    #h = 3.0
+    #r = 44.7
+    #t = 7.0
+    #s = 1.5
+    #c = 5.0
+
+    #rf_central_conductor_width = 21.0
+
+    # taken from source code, find number of tcells to calculate ls
+    #num_cells = np.floor(modulation_length / (tl + tc))
+
+    #rf_central_conductor_width = h_racetrack - rf_gap - 2*s - 2*h     # w, adjusted for optical waveguide bend radius
+    #h_racetrack = rf_gap + 2*s + 2*h + rf_central_conductor_width
+
+    #define subcomponents
+    u_bend_racetrack = lnoi400.cells.U_bend_racetrack(
+        v_offset = h_racetrack,
+        p = 1.0,
+        with_arc_floorplan = True,
+        cross_section = RT_cross_section,
+    )
+
+    ecoup = mpl.asymmetric_directional_coupler_racetrack(
+        cross_section_io=RT_cross_section,
+        cross_section_bus=DC_cross_section,
+        io_wg_sep = DC_io_wg_sep*2,
+        sbend_length = DC_sbend_length,
+        central_straight_length = DC_central_straight_length,
+        coupl_wg_sep = DC_coupl_wg_sep,
+        coup_wg_width = DC_coup_wg_width,
+    )
+    ec_xsize = ecoup.xsize
+
+    dc_wg_sep = gf.components.straight(
+        length = DC_ubend_sep,
+        cross_section=RT_cross_section,
+    )
+    dc_wg_shorter = gf.components.straight(
+        length = ls+2*lextra-ec_xsize-DC_ubend_sep,
+        cross_section=RT_cross_section,
+    )
+
+    dc_wg = gf.components.straight(
+        length = ls+2*lextra,
+        cross_section=RT_cross_section,
+    )
+
+
+
+    # create the Tcell component with NO GSG LANDING
+    rf_line = lnoi400.cells.trail_cpw(
+        length = modulation_length,
+        signal_width = rf_central_conductor_width,
+        gap_width = rf_gap,
+        th = h,            #h
+        tl = r,            #r
+        tw = t,            #t
+        tt = s,            #s
+        tc = c,            #c
+        ground_planes_width = 180.0,
+        rounding_radius = 0.5,
+        bondpad = {
+                    "component": "CPW_pad_linear",
+                    "settings": {
+                    "start_width": 80.0,
+                    "length_straight": 10.0,
+                    "length_tapered": 190.0,
+            },
+        }
+        )
+
+    def racetrack():
+        # push subcomponents to design
+        race_track = gf.Component()
+        u_bend_ref1 = race_track << u_bend_racetrack
+        u_bend_ref2 = race_track << u_bend_racetrack
+        straight_ref1 = race_track << dc_wg_shorter
+        straight_sep_ref = race_track << dc_wg_sep
+        ec_top = race_track << ecoup
+        straight_ref2 = race_track << dc_wg
+
+
+        u_bend_ref1.connect("o2", straight_ref1.ports["o2"])
+        straight_ref2.connect("o2", u_bend_ref1.ports["o1"])
+        ec_top.connect("o3", straight_ref1["o1"])
+        straight_sep_ref.connect("o1", ec_top.ports["o4"])
+        u_bend_ref2.connect("o1", straight_sep_ref.ports["o2"])
+        #u_bend_ref2.connect("o1", ec_top.ports["o4"])
+        u_bend_ref2.connect("o2", straight_ref2.ports["o1"])
+
+        exposed_ports = [
+            ("o1", ec_top.ports["o1"]),
+            ("o2", ec_top.ports["o2"])
+        ]
+        [race_track.add_port(name=name, port=port) for name, port in exposed_ports]
+        #race_track.flatten()
+
+        return race_track
+
+    
+    # create assembly
+    c = gf.Component()
+
+    race_track_top = c << racetrack()
+    race_track_bottom = c << racetrack()
+    race_track_bottom.dmirror_y()
+    rf_line_ref = c << rf_line
+    
+    #define position of rf_line
+
+    gap_eff = rf_gap + 2 * np.sum(
+        [rf_line_ref.cell.settings[key] for key in ("tt", "th") if key in rf_line_ref.cell.settings]
+    )
+    #print(gap_eff)
+
+    rf_line_ref.dmove(
+        (rf_line_ref.ports["e1"].dcenter[0]*0.5 + rf_line_ref.ports["e2"].dcenter[0]*0.5,0),
+        (race_track_top.center[0],0.0),
+    )
+
+    race_track_top.dmove((0, h_racetrack + rf_central_conductor_width/2 + s + h + rf_gap/2))
+    race_track_bottom.dmove((0, -h_racetrack-(rf_central_conductor_width/2 + s + h + rf_gap/2)))
+
+    #Add rf_line ports to system
+    for name, port in [
+        ("e1", rf_line_ref.ports["bp1"]),
+        ("e2", rf_line_ref.ports["bp2"]),
+    ]:
+        c.add_port(name=name, port=port)   
+    
+    # Expose the ports
+    exposed_ports = [
+        ("o1", race_track_top.ports["o1"]),
+        ("o2", race_track_top.ports["o2"]),
+        ("o3", race_track_bottom.ports["o1"]),
+        ("o4", race_track_bottom.ports["o2"]),
+    ]
+
+    [c.add_port(name=name, port=port) for name, port in exposed_ports]
+
+    return c
 
 #####################
 # 30GHz FSR Racetrack Resonator (Final Design)
